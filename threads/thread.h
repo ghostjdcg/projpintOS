3aadb0b00e1234a5ca4db85e791997a91149e284
ghostjdcg <josuedavid.contreras@galileo.edu>
2023-02-14 23:28:26 +0000

    Alarm implementation

--- threads/thread.h
+++ threads/thread.h
@@ -92,6 +92,7 @@ struct thread
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
+    uint64_t threst;
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
